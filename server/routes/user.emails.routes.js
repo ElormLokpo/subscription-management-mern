6bc341972248d71c90260c027b32e860a4458a71
ElormLokpo <benedictkomla@gmail.com>
2023-02-01 12:32:52 +0000

    Authorization complete 85%

--- server/routes/user.emails.routes.js
+++ server/routes/user.emails.routes.js
@@ -6,11 +6,12 @@ const {
     createEmailsController,
     updateEmailController,
     deleteEmailController
-} = require('../controllers/user.emails.controller')
+} = require('../controllers/user.emails.controller');
+const { adminAuthorize } = require('../middleware/admin.auth')
 
-Router.route('/').get(getEmailsController);
+Router.route('/').get(adminAuthorize, getEmailsController);
 Router.route('/create').post(createEmailsController);
-Router.route('/:emailid').get(getEmailController).put(updateEmailController).delete(deleteEmailController);
+Router.route('/:emailid').get(getEmailController).put(adminAuthorize,updateEmailController).delete(adminAuthorize, deleteEmailController);
 
 
 module.exports = Router;
