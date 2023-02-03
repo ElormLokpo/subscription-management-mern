6074b0d9c968d1a024dcc216cc02fcd3107bf09f
ElormLokpo <benedictkomla@gmail.com>
2023-02-03 13:02:04 +0000

    Auth screens ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './assets/styles/main.scss';
+import SignIn from './pages/auth/signin';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import SignUp from './pages/auth/signup';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<SignUp />, document.querySelector('#root'));
---
+++ client/src/pages/auth/signup/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='h-full grid grid-cols-3 main-signup-container'>
+        <div className=''>
+            s
+        </div>
+        <div className='col-span-2 right-pane flex justify-center items-center'>
+            <div className='bg-gray-900 w-2/5 rounded p-3'>
+                <h2 className='text-2xl mb-2'>Sign Up</h2>
+                <p className='text-sm font-light'>It is a long established fact that a reader will be distracted by 
+                    the readable content of a page when  
+                </p>
+
+                <div className='mt-4'>
+                    <div className='flex flex-col mb-3'>
+                        <label className='text-xs mb-1'>Fullname:</label>
+                        <input type = 'text' className='py-1 rounded bg-gray-700' />
+                    </div>
+
+                    <div className='flex flex-col mb-3'>
+                        <label className='text-xs mb-1'>Email:</label>
+                        <input type = 'email' className='py-1 rounded bg-gray-700' />
+                    </div>
+
+                    <div className='flex flex-col mb-3'>
+                        <label className='text-xs mb-1'>Password:</label>
+                        <input type = 'password' className='py-1 rounded bg-gray-700' />
+                    </div>
+
+                    <div className='flex flex-col mb-5'>
+                        <label className='text-xs mb-1'>Confirm Password:</label>
+                        <input type = 'password' className='py-1 rounded bg-gray-700' />
+                    </div>
+
+                    <div className='w-full my-5'>
+                        <button className='btn hover:bg-yellow-700 w-full py-2 rounded text-sm'>Sign Up</button>
+                    </div>
+                    {/* <p className='flex justify-center my-2'>
+                        OR
+                    </p>
+                    <div className='w-full'>
+                        <button className='bg-blue-600 w-full py-2 rounded text-sm'>Continue with Google</button>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SignUp;
